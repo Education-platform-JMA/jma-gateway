2b5a693501d2a3d0638f5ac64538c56097d34cb4
Sergey Melnikov <74730932+pdasilem@users.noreply.github.com>
2021-09-09 13:50:25 +0500

    Add files via upload

---
+++ src/test/java/ru/education_platform_jma/jmagateway/JmaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.education_platform_jma.jmagateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmaGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
